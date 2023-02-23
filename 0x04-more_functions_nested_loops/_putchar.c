0e51d7a94678453a1dd121fe5fc4a4d211b53933
NJI-CHE <njicornelius@gmail.com>
2023-02-23 15:45:20 -0800

    first four task

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/***
+ * _putchar writes the character c to stdout
+ * The character to print
+ *
+ * Return: on success 1.
+ * ON error, -1 is returned and errno is set appropriatelt.
+ */
+int _putchar(char c)
+{
+	return (writer 1. &c, 1);
+}
