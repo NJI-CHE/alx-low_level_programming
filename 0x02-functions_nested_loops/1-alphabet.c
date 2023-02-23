25ce77d28dd8afbf13888e307dea348b9ac1f28e
NJI-CHE <njicornelius@gmail.com>
2023-02-23 03:12:38 -0800

    alphabet fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,3 @@
-@@ -1,14 +1,15 @@
-#include "holberton.h"
 #include "main.h"
 
 /**
@@ -7,10 +5,6 @@
  */
 void print_alphabet(void)
 {
-	char letter;
-
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
 	char lc;
 
 	for (lc = 'a'; lc <= 'z'; lc++)
